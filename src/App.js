8f4e4a0de84ff9ed5400e69f137c54340c7add40
dashingshree6 <dashingshree6@gmail.com>
2021-08-26 21:20:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,31 +4,49 @@ import CardList from "./components/Card/CardList";
 import 'tachyons';
 import {series} from './lib/Series';
 import Searchbox from './components/Searchbox/Searchbox';
+import Scroll from './components/Scroll/Scroll'
 
 class App extends Component {
  constructor() {
    super()
    this.state = {
-     series : series, 
+    //series : [], 
+     series : series,
      setInput : ''
    }
  }
-  
+ 
+ /*componentDidMount() {
+   fetch('./lib/Series.js')
+   .then(response => response.json())
+   .then(series => this.setState({series : series}))
+ }
+ */
+
   onSearch = (event) => {
     this.setState({setInput: event.target.value})
   }
   render() {
-    const displaySeries = this.state.series.filter(series => {
-      return series.name.toLowerCase().includes(this.state.setInput.toLowerCase())
+    const {series, setInput} = this.state ;
+    const displaySeries = series.filter(series => {
+      return series.name.toLowerCase().includes(setInput.toLowerCase())
     })
-    return (
-      <div className="App">
-        <h1>IMDB</h1>
-        <Searchbox searchChange={this.onSearch}/>  
-        <CardList series={displaySeries}/> 
-      </div>
-    );
-  }
+    if(series.length === 0){
+   //if(!series.length)   
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="App">
+          <h1>IMDB</h1>
+          <Searchbox searchChange={this.onSearch}/>  
+          <Scroll>
+            <CardList series={displaySeries}/> 
+          </Scroll>
+        </div>
+      );
+  
+    }
+      }
 }
 
 export default App;
