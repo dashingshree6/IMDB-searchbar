fb9518fdc2cd3222deeb02a35dd7e77a12a31c7b
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2021-10-06 21:08:33 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import './Card.css'
 
-const Card = ({src, title, category, description}) => {
-    return (
-        <div className='w-30 h-m bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='series' src={src} />
-            <div>
-                <h2>{title}</h2>
-                <span><strong>{category}</strong></span>
-                <p>{description}</p>
-            </div>
-        </div>
-    )
-}
+
 
 export default Card;
