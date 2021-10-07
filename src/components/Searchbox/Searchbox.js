cf3a2f64c76186bd514bce87e248adf234595a33
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2021-10-07 18:46:56 +0530

    Update Searchbox.js

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -1,14 +1,6 @@
 import React from 'react';
 
 const Searchbox = ({setInput, searchChange}) => {
-    return (
-       <div className='pa2'>
-           <input
-           className='pa3 ba b--green bg-lightest-blue' 
-           type="search" 
-           placeholder="Enter the series name"
-           onChange={searchChange}/>
-       </div> 
-            )
+   
 }
 export default Searchbox;
