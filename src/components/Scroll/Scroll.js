5c0b81b5739698d85880eed6eb59b96725b5e390
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2021-10-08 20:06:56 +0530

    Update Scroll.js

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Scroll.css'
 
 const Scroll = (props) => {
- 
+     return (
+        <div className='scroll'>
+            {props.children}
+        </div>
+    )
     
 }
 
